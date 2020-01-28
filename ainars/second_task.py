5b5041c3a7787418e048a2894d12207a44aadc3b
Ainars Palcevskis <ainars.palcevskis@gmail.com>
2020-01-28 13:07:29 +0200

    Edited for cleaner usage
    Now it shows list of actions only one time, and if wrong answer it
    explains what needs to be entered for correct execution.

--- ainars/second_task.py
+++ ainars/second_task.py
@@ -2,16 +2,12 @@ name = raw_input("What is yout name? ")
 print("Hello {}".format(name))
 
 d = {'action 1': 'read book', 'action 2': 'practice with SDx', 'action 3': 'sleep'}
+print("Please choose your action:\n{}".format(d))
 
-while True:    
-    print("Please choose your action:\n{}".format(d))
+while True:
     choice = raw_input("Your choice? ")
     if choice in d.keys():        
-        print("I'll {} later".format(d.get(choice)))
+        print("I'll {} later.".format(d.get(choice)))
         break
     else:
-        print("Wrong action!")
-              
-        
-
-        
+        print("Wrong action! You need to write action and number from the list above.")
