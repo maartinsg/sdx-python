00856d8ec293b6df5e14dd9ad4bcc4417770327f
Martins Galenieks tested <sourcetester@gmail.com>
2020-01-28 09:44:58 +0200

    I created one maartinsg/python.sample.1.txt.py test /usr/bin/python3 file, TA

---
+++ maartinsg/python.sample.1.txt.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3 
+
+# To print three lines of the text as a sample from copycat
+# to inform maybe about the development and CD/CI methods
+
+
+print('''Python is a programming language 
+that lets you work quickly 
+and integrate systems more effectively.''')
