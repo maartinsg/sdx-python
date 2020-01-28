a47b4a0ea5374d13d72b49e50f75070e259a9b6d
Raivis Ozols <rozols8@gmail.com>
2020-01-28 15:56:42 +0200

    using classes now, more safely

--- raivis/using_clases.py
+++ raivis/using_clases.py
@@ -1,7 +1,7 @@
 # class definiton
 class Rectangle:
     def __init__(self, name, a, b):
-        self.name = name
+        self.__name = name
         self.a = a
         self.b = b
 
@@ -9,12 +9,19 @@ class Rectangle:
         return self.a*2+self.b*2
     def area(self):
         return self.a*self.b
+    def getName(self):
+        return self.__name
+    def setName(self, arg):
+        self.__name = arg
+
+    
 #instances
 r1 = Rectangle('kitchen', 2,4)
 r2 = Rectangle('hall', 1,3)
+r1.setName("ABC")
 
 for r in [r1, r2]:
-    print("{} sides: {}, {}".format(r.name, r.a, r.b))
-    print("{} perimeter: {}".format(r.name, r.perimeter()))
-    print("{} area: {}".format(r.name, r.area()))
+    print("{} sides: {}, {}".format(r.getName(), r.a, r.b))
+    print("{} perimeter: {}".format(r.getName(), r.perimeter()))
+    print("{} area: {}".format(r.getName(), r.area()))
 
