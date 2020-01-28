78a6dcaa733868248ea11011f41515e577e06772
Martins Galenieks tested <sourcetester@gmail.com>
2020-01-28 14:29:44 +0200

    I created the maartinsg/sample-positional-arguments-1-txt.py example file, TA

---
+++ maartinsg/sample-positional-arguments-1-txt.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+def getSum(*args):
+    total = 0
+    for n in args:
+        total += n
+    return total
+
+a0 = getSum()
+a3 = getSum(2, 30, 4)
+a4 = getSum(10, 2, 13, 44)
+
+print("a0: %s" % a0)
+print("a3: %s" % a3)
+print("a4: %s" % a4)
