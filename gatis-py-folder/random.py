f9fa0dc87cb5782857ee08c3f06029c4f2c925f6
Gatis <student@ubuntu.com>
2020-01-28 14:05:38 +0200

    game

---
+++ gatis-py-folder/random.py
@@ -0,0 +1,15 @@
+import random
+secret = random.randint(1,10)
+print secret
+
+while True:
+    guess = int(input("Guess a number between 1 and 10: "))
+    if guess < secret:
+        print("Too low :( ")
+    elif guess > secret:
+        print("Too High :( ")
+    else:
+        print ("You Win! :)")
+        break
+
+
