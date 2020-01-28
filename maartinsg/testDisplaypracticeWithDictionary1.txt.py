97d49f855e4728bc1587a4f60976ab6401d9fed8
Martins Galenieks tested <sourcetester@gmail.com>
2020-01-28 13:02:50 +0200

    I modified as the 4th the maartinsg/testDisplaypracticeWithDictionary1.txt.py file like to test raw_input, use Dictionary variables from Python and so on probably.

--- maartinsg/testDisplaypracticeWithDictionary1.txt.py
+++ maartinsg/testDisplaypracticeWithDictionary1.txt.py
@@ -3,10 +3,15 @@
 # I have written the code for the action `simulation` of this Python program script
 # as follows for the Python v.2.7.13 with raw_input:
 
+#to get acquainted politely 
 s = raw_input("What is your name: ")
 print("Hello " + s)
+
+#to assign dictionary list like
 h = { 'action 2': 'practice with SDx', 'action 3': 'sleep', 'action 1': 'read book' }
 print("Please choose your action: ")
+
+#to confirm choices from the STDIN
 s = raw_input("Your choice? ")
-print("I choose the action of " s)
+print("I choose the action of " + s)
 
