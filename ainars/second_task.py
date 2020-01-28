2dd3c435df188aab5b9876547c860944336d3bbd
Ainars Palcevskis <ainars.palcevskis@gmail.com>
2020-01-28 11:59:03 +0200

    made error text if wring answer

--- ainars/second_task.py
+++ ainars/second_task.py
@@ -3,4 +3,11 @@ print("Hello {}".format(name))
 d = {'action 1': 'read book', 'action 2': 'practice with SDx', 'action 3': 'sleep'}
 print("Please choose your action:\n{}".format(d))
 choice = raw_input("Your choice? ")
-print("I'll {} later".format(d.get(choice)))
+if choice in d.keys():
+        print("I'll {} later".format(d.get(choice)))
+else:
+        print("Wrong action!")
+              
+        
+
+        
