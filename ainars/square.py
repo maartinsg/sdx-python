2cda0e71428fb35501e5a50575a3954299565f50
Ainars Palcevskis <ainars.palcevskis@gmail.com>
2020-01-28 13:50:11 +0200

    same but while loop with counter

--- ainars/square.py
+++ ainars/square.py
@@ -1,8 +1,8 @@
 import turtle
-for steps in range(4):
+
+counter = 1
+while counter <= 4:
     turtle.forward(100)
     turtle.right(90)
-    for moresteps in range(4):
-        turtle.forward(50)
-        turtle.right(90)
+    counter = counter + 1
 turtle.done()
