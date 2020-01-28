1347ad8089c197a8aea3b24da52e6135ef0fc071
Raivis Ozols <rozols8@gmail.com>
2020-01-28 13:32:30 +0200

    Added friendly user input

--- raivis/dicti_prac.py
+++ raivis/dicti_prac.py
@@ -3,11 +3,11 @@ print("Hello " + name)
 dicti = {'action 2': 'practice with SDx', 'action 3': 'sleep', 'action 1': 'read book'}
 print("Please choose your action: {}".format(dicti))
 input=raw_input("Your choice? ")
-if (input==str('action 2')):
+if (input.lower()==str('action 2')):
    print("I'll " + str(dicti.get('action 2')) + " later")
-elif (input==str('action 1')):
+elif (input.lower()==str('action 1')):
    print("I'll " + str(dicti.get('action 1')) + " later")
-elif (input==str('action 3')):
+elif (input.lower()==str('action 3')):
    print("I'll " + str(dicti.get('action 3')) + " later")
 else:
    print("Sorry, wrong input!")
