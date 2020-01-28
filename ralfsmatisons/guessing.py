e8156b39131627e5b2abf53e4f9160b75550ce65
Ralfs Matisons <ralfs.matisons99@gmail.com>
2020-01-28 13:57:44 +0200

    ffff

--- ralfsmatisons/guessing.py
+++ ralfsmatisons/guessing.py
@@ -4,5 +4,9 @@ value=raw_input("Enter a value between 1 and 10 ")
 rnd=random.randint(1,10)
 if (value== rnd):
     print("You have guessed the value")
+elif(value > rnd):
+    print("Too high")
+elif(value< rnd):
+    print("Too low :C ")
 else:
-    print("Didnt guess")
+    print("You win")
