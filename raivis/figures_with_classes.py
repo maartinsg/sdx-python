eadd41b77998dc2c9c9a924591a1b24f5494ae38
Raivis Ozols <rozols8@gmail.com>
2020-01-28 16:11:49 +0200

    using classes again

---
+++ raivis/figures_with_classes.py
@@ -0,0 +1,27 @@
+import math
+
+#class definiton
+class Figure:
+    def __init__(self, name):
+        self.name = name
+class Square(Figure):
+    def __init__(self, name, a):
+        Figure.__init__(self, name)
+        self.a = a
+        
+    def area(self):
+        return self.a*self.a
+class Circle(Figure):
+    def __init__(self, name, r):
+        Figure.__init__(self, name)
+        self.r = r
+        
+    def area(self):
+        return math.pi*self.r**2
+
+# Instances
+s1 = Square('room', 5)
+c1 = Circle('pool', 2)
+
+for r in [s1, c1]:
+    print("{} area: {}".format(r.name, r.area()))
