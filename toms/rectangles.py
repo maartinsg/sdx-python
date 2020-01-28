ffb601baeabe0fc59e8bc3786b6bad63b682be6b
Toms Koralis <tomskoralis@inbox.lv>
2020-01-28 16:08:44 +0200

    rectangle and square classes

--- toms/rectangles.py
+++ toms/rectangles.py
@@ -1,22 +1,29 @@
 class rectangle:
     def __init__(self, name, a, b):
         self.name = name
-        self.__a = a
-        self.__b = b
+        self.a = a
+        self.b = b
     def perimeter(self):
-        return self.__a * 2 + self.__b * 2
+        return self.a * 2 + self.b * 2
     def area(self):
-        return self.__a * self.__b
+        return self.a * self.b
     def setName(self, x):
         self.name = x
         
-
-r1 = rectangle ("1st", 2, 3)
-r2 = rectangle ("2nd", 6, 4)
-
+class square(rectangle):
+    def __init__(self, name, a):
+        self.name = name
+        self.a = a
+        self.b = a
+    def setName(self, x):
+        self.name = x
+        
+r1 = rectangle ("sq1", 2, 3)
+r2 = rectangle ("sq2", 6, 4)
+s1 = square ("sq1", 4)
 r1.setName("ABC")
 
-for r in [r1, r2]:
+for r in [r1, r2, s1]:
     print("Rectangle name: " + r.name)
     print("Rectangle perimeter: {}".format(r.perimeter()))
     print("Rectangle area: {}" .format(r.area()))
