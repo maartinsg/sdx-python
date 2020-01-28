b301ed78ce083cee3c59e60fe2c672145236dc1c
Anna D <dobritskaya39.ab@gmail.com>
2020-01-28 16:04:43 +0200

    inheritance is added

--- Anna/class_practice.py
+++ Anna/class_practice.py
@@ -17,12 +17,19 @@ class Rectangle:
     def setName(self, arg):
         self.__name = arg
 
+        
+class Square(Rectangle):
+    def __init__(self, name, a):
+        Rectangle.__init__(self, name, a, a)
+
 r1 = Rectangle("kitchen", 2, 4)
 r2 = Rectangle("hall", 1, 3)
+s1 = Square('room', 5)
+s2 = Square('bath', 2)
 
 r1.setName("room")
 
-for r in [r1, r2]:
+for r in [r1, r2, s1, s2]:
     print("{} sides: {}, {}".format(r.getname(), r.a, r.b))
     print("{} area: {}".format(r.getname(), r.area()))
     print("{} perimeter: {}".format(r.getname(), r.perimeter()))
