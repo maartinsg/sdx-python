8e952d8dd79f76078ca1b64fa6f13b1a2f4b9e1a
Ainars Palcevskis <ainars.palcevskis@gmail.com>
2020-01-28 12:02:23 +0200

    made loop
    doesnt end if entered wrong value

--- ainars/second_task.py
+++ ainars/second_task.py
@@ -1,11 +1,15 @@
 name = raw_input("What is yout name? ")
 print("Hello {}".format(name))
+
 d = {'action 1': 'read book', 'action 2': 'practice with SDx', 'action 3': 'sleep'}
-print("Please choose your action:\n{}".format(d))
-choice = raw_input("Your choice? ")
-if choice in d.keys():
+
+while True:    
+    print("Please choose your action:\n{}".format(d))
+    choice = raw_input("Your choice? ")
+    if choice in d.keys():        
         print("I'll {} later".format(d.get(choice)))
-else:
+        break
+    else:
         print("Wrong action!")
               
         
