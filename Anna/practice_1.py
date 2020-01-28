eb442fcb3d97e8eaf50dff5e73e77ed322f11323
Anna D <dobritskaya39.ab@gmail.com>
2020-01-28 11:54:44 +0200

    practice_1

--- Anna/practice_1.py
+++ Anna/practice_1.py
@@ -5,4 +5,4 @@ print("Hello " + name)
 actions = {'action 2':'practice with SDx', 'action 3':'sleep', 'action 1':'read book'}
 print("Please choose your action:\n{}".format(actions))
 choice = raw_input("Your choice? ")
-print("I'll {} later".format(actions[choice]))
+print("I'll {} later".format(actions.get(choice,'try to answer this question')))
