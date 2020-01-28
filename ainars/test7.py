6548082131a151f4716e7cd2e097bbe83668ddc2
Ainars Palcevskis <ainars.palcevskis@gmail.com>
2020-01-28 14:59:59 +0200

    example with try and except

---
+++ ainars/test7.py
@@ -0,0 +1,22 @@
+while True:
+    try:
+        first = raw_input("Number 1: ")
+        second = raw_input("Number 2:" )
+
+        result = float(first) / float(second)
+        print("{} / {} = {}".format(first, second, result))
+        errorFlag = False
+    except ZeroDivisionError:
+        print("The answer is infinity")
+        errorFlag = True
+
+    except Exception as e:
+        print("Unexpected error ({})".format(e.message))
+        break
+
+    finally:
+        print("Always executed!")
+
+    if not errorFlag:
+        print("This message only displays if there is no error!")
+print("Good buy!")
