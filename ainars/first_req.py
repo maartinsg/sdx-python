2225a68d4786896e69445ff0a890c4e2e712f9e4
Ainars Palcevskis <ainars.palcevskis@gmail.com>
2020-01-28 17:05:03 +0200

    final commit of day

---
+++ ainars/first_req.py
@@ -0,0 +1,9 @@
+import requests
+
+url = 'http://www.google.com'
+r = requests.get(url)
+print("URL: " + str(url))
+print("Status: " + str(r.status_code))
+print("Header: " + str(r.headers['content-type']))
+print("Encoding: " + str(r.encoding))
+print("Body: {:.200}: ".format(r.text))
