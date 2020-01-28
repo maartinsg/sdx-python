f18f17b57e27d2394c98270ba199c27c601eddac
Ralfs Matisons <ralfs.matisons99@gmail.com>
2020-01-28 12:02:10 +0200

    huuge f

---
+++ ralfsmatisons/thatwork.py
@@ -0,0 +1,18 @@
+
+
+#askings Jevs name
+name=raw_input("What's ur name ") #python3 is cooler with just input
+print("Hello" + " " + name )
+
+#2nd part
+h={"action2": "practice with SDx", "action3": "sleep", "action1":"read a book"}
+
+print( "Please choose your action :{} ".format(h))
+choice=raw_input("What do you choose")
+
+print("I'll : {}".format(h[choice]) + " " +"later")
+#print("I'll" + " "+  + "later")
+
+       
+
+
