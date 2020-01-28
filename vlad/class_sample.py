1ae914cfc019c5487496431a2a1b9992210bfaba
vladislavs ignatenkovs <phontyz@gmail.com>
2020-01-28 16:02:40 +0200

    Added class Square to class_sample.py

--- vlad/class_sample.py
+++ vlad/class_sample.py
@@ -17,14 +17,23 @@ class Rectangle:
     def getName(self):
         return self.__name
 
+class Square(Rectangle):
+    def __init__(self, name, a):
+        Rectangle.__init__(self, name, a, a)
+
     
 # Instances
 r1 = Rectangle('kitchen', 2,4)
 r2 = Rectangle('hall', 1,3)
-r1.setName("ABC")
+s1 = Square('room', 5)
+s2 = Square('bath', 2)
+#r1.setName("ABC")
 
-for r in (r1, r2):
+for r in (r1, r2, s1, s2):
     print("{} sides: {}, {}".format(r.getName(), r.a, r.b))
     print("{} perimeter: {}".format(r.getName(), r.perimeter()))
     print("{} area: {}".format(r.getName(), r.area()))
+
+
+
 
