21b905ef98afc232f4a0cb36dbd992806453f19b
Raivis Ozols <rozols8@gmail.com>
2020-01-28 16:02:19 +0200

    using classes now, more safely, and with subclass

--- raivis/using_clases.py
+++ raivis/using_clases.py
@@ -14,13 +14,18 @@ class Rectangle:
     def setName(self, arg):
         self.__name = arg
 
-    
+class Square(Rectangle):
+        def __init__(self, name, a):
+            Rectangle.__init__(self, name, a, a)
 #instances
 r1 = Rectangle('kitchen', 2,4)
 r2 = Rectangle('hall', 1,3)
+s1 = Square('room', 5)
+s2 = Square('bath', 2)
+
 r1.setName("ABC")
 
-for r in [r1, r2]:
+for r in [r1, r2, s1, s2]:
     print("{} sides: {}, {}".format(r.getName(), r.a, r.b))
     print("{} perimeter: {}".format(r.getName(), r.perimeter()))
     print("{} area: {}".format(r.getName(), r.area()))
