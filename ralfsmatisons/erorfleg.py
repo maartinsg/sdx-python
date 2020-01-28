8912af7cd86cd7f3f27253a1ad968fadd5ad8740
Ralfs Matisons <ralfs.matisons99@gmail.com>
2020-01-28 15:06:46 +0200

    except not optional

---
+++ ralfsmatisons/erorfleg.py
@@ -0,0 +1,21 @@
+while True:
+    try:
+        first=raw_input("Number 1  ")
+        second=raw_input("NUmber2  ")
+
+        result = float(first)/float(second)
+        print("{} / {} = {}".format(first,second,result))
+        errorFlag=False
+    except ZeroDivisionError:
+        print("THe answer is infinity")
+        errorFlag=True
+        raise()
+    except Exception as e:
+        print("Unexpected error (())".format(e.message))
+        break
+    finally:
+        print("Always executed")
+
+    if not errorFlag:
+        print("This message only displays if no error")
+print("Good bye")
