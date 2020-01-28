2b2a7dc9f7d4fd0b21c111bc9f375a5ce84bc1d0
Anna D <dobritskaya39.ab@gmail.com>
2020-01-28 16:39:21 +0200

    another library

---
+++ Anna/lib_subprocess.py
@@ -0,0 +1,15 @@
+from subprocess import Popen, PIPE
+
+#execute command
+process = Popen (" ".join(['ping', '8.8.8.8', '-c', '2']),
+                 shell=True, stdout=PIPE, stderr=PIPE)
+
+#read STDOUT
+print(process.stdout.read())
+
+#wait for child process to terminate
+rc = process.wait()
+print("exit code: {}".format(rc))
+
+#read STDERR
+print("error: {}".format(process.stderr.read()))
