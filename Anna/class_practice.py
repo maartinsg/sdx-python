5b2fb50c82a27811e15194d21d8cb293958c24ce
Anna D <dobritskaya39.ab@gmail.com>
2020-01-28 15:40:03 +0200

    practice with OOP

---
+++ Anna/class_practice.py
@@ -0,0 +1,20 @@
+# class definition
+class Rectangle:
+    def __init__(self, name, a, b):
+        self.name = name
+        self.a = a
+        self.b = b
+
+    def perimeter(self):
+        return self.a*2+self.b*2
+
+    def area(self):
+        return self.a*self.b
+
+r1 = Rectangle("kitchen", 2, 4)
+r2 = Rectangle("hall", 1, 3)
+
+for r in [r1, r2]:
+    print("{} sides: {}, {}".format(r.name, r.a, r.b))
+    print("{} area: {}".format(r.name, r.area()))
+    print("{} perimeter: {}".format(r.name, r.perimeter()))
