3385c67f1a3c99cc2858b987d52ba9e0dcc7006d
Toms Koralis <tomskoralis@inbox.lv>
2020-01-28 13:02:21 +0200

    practice and stringtest update with commentary

--- toms/practice.py
+++ toms/practice.py
@@ -1,8 +1,13 @@
+#1st input
 nameInput = raw_input("What is your name? ")
 print("Hello " + nameInput)
 print("Please choose your action:")
+
+#dictionary of available choices:
 d1 = { 'action 2': 'practice with SDx','action 3': 'sleep', 'action 1':'read book'}
 print("{}".format(d1))
+
+#2nd input
 choiceInput = raw_input("Your choice:")
 if (d1.has_key(choiceInput)):
     print("I'll '{}' later".format(d1.get(choiceInput, None)) )
--- toms/stringtest.py
+++ toms/stringtest.py
@@ -2,4 +2,25 @@
 
 print('''This is a \'
 text in\n\'multiple lines that has\nsome
-\' characters\" ''')
+\' characters\" \n''')
+
+#test for lists
+
+z = [1, 5.5, "some text", True]
+print("{}".format(z))
+
+#test for list operations
+z.append("to replace")
+print("{}".format(z))
+z[4] = "replaced"
+print("{}".format(z))
+
+#test for tuple
+x = (1, 5.5, "some text", True)
+print("{}".format(x))
+
+#test for dictionary
+c = { "id":12, "name":"Python"}
+print("{}".format(c))
+c["name"] = "Toms"
+print("{}".format(c.get("name",None)))
