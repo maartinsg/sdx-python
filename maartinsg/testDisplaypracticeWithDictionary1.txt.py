e4e3966d3b1aef81005c655afea7d488b539850a
Martins Galenieks tested <sourcetester@gmail.com>
2020-01-28 12:59:20 +0200

    I modified one maartinsg/testDisplaypracticeWithDictionary1.txt.py file like to test raw_input, use Dictionary variables from Python and so on probably.

--- maartinsg/testDisplaypracticeWithDictionary1.txt.py
+++ maartinsg/testDisplaypracticeWithDictionary1.txt.py
@@ -4,14 +4,9 @@
 # as follows for the Python v.2.7.13 with raw_input:
 
 s = raw_input("What is your name: ")
-print(s)
 print("Hello " + s)
-print("Please choose your action: ")
 h = { 'action 2': 'practice with SDx', 'action 3': 'sleep', 'action 1': 'read book' }
+print("Please choose your action: ")
 s = raw_input("Your choice? ")
-#incompatible code
-#if s not NULL
-#    print('I'll '+ s +'maybe later')
-#else:
-#    exit(9)
-    
+print("I choose the action of " s)
+
