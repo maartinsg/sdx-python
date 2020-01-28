3f41fce42c03e3a68b6619bb44874cc9e9108a0d
vladislavs ignatenkovs <phontyz@gmail.com>
2020-01-28 13:43:16 +0200

    moving_turtle changed

--- vlad/moving_turtle.py
+++ vlad/moving_turtle.py
@@ -1,10 +1,6 @@
 #turtle does square
 import turtle
-turtle.forward(100)
-turtle.right(90)
-turtle.forward(100)
-turtle.right(90)
-turtle.forward(100)
-turtle.right(90)
-turtle.forward(100)
+for steps in range(4):
+    turtle.forward(100)
+    turtle.right(90)
 turtle.done()
