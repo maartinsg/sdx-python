70620b69e8b99a2c53e1ad7c730c2141aee451d9
vladislavs ignatenkovs <phontyz@gmail.com>
2020-01-28 15:46:28 +0200

    Tried to hide values

--- vlad/class_sample.py
+++ vlad/class_sample.py
@@ -1,7 +1,7 @@
 #class definition
 class Rectangle:
     def __init__(self, name, a, b):
-        self.name = name
+        self.__name = name
         self.a = a
         self.b = b
 
@@ -16,7 +16,7 @@ r1 = Rectangle('kitchen', 2,4)
 r2 = Rectangle('hall', 1,3)
 
 for r in (r1, r2):
-    print("{} sides: {}, {}".format(r.name, r.a, r.b))
-    print("{} perimeter: {}".format(r.name, r.perimeter()))
-    print("{} area: {}".format(r.name, r.area()))
+    print("{} sides: {}, {}".format(r.__name, r.a, r.b))
+    print("{} perimeter: {}".format(r.__name, r.perimeter()))
+    print("{} area: {}".format(r.__name, r.area()))
 
