01643767d1004a2b3e459967e024e78804bfd605
Martins Galenieks tested <sourcetester@gmail.com>
2020-01-28 11:03:49 +0200

    I modified earlier as a second commit probably the maartinsg/print.bash.env.os.from.python.txt.1.py file

--- maartinsg/print.bash.env.os.from.python.txt.1.py
+++ maartinsg/print.bash.env.os.from.python.txt.1.py
@@ -9,3 +9,7 @@ import os
 # I wrote a text to display HOME variable from Bash likes
 sHOME = os.environ["HOME"]
 print(sHOME)
+print()
+# I wrote a test to display PATH variable from Bash likes
+sPATH= os.environ["PATH"]
+print(sPATH)
