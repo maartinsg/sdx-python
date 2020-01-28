11f8191973e44a09d7c69084647fa55943b82f71
Martins Galenieks tested <sourcetester@gmail.com>
2020-01-28 09:58:05 +0200

    I created one maartinsg/a.sampled.multiline.separateprintfunctions.1.py and tried to verify again....

---
+++ maartinsg/a.sampled.multiline.separateprintfunctions.1.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+# This is just to test print for three lines with 3 print() functions 
+# This is just a test also written by me, TA, comprehensible.
+
+print('Python is a programming language')
+print('that lets you work quickly ')
+print('and integrate systems more effectively\n')
+print('\nalso with the Ansible, Jenkins, Terraform, etc., comprehensible.')
+print('I have been testing a system in the past. TA. Comprehensible.\n')
