9608c2782f307be5496a17cfa660ed4678175b06
Imants Purins <itsimants@gmail.com>
2020-01-29 12:24:46 +0200

    imants-feature-1

--- imants/restapid/restp1.py
+++ imants/restapid/restp1.py
@@ -2,18 +2,25 @@
 from flask import Flask
 from flask_restful import reqparse, abort, Api, Resource
 
-class TodoSimple(Resource):
+class TodoList(Resource):
     def get(self):
         print("debug: sending full list")
         return TODOS
     
     def put (self):
-        args = parse.parse_args()
+        args = parser.parse_args()
         todo_id = int(max(TODOS.keys()).lstrip('todo')) + 1
         todo_id = 'todo%i' % todo_id
         TODOS[todo_id] = {'task': args['task']}
         print("debug: added task with id'{}'".format(todo_id))
         return TODOS[todo_id], 201
+
+class Todo(Resource):
+    def get(self, todo_id):
+        # abort_if_todo_doesnt_exist(todo_id)
+        return TODOS[todo_id]
+    
+ 
     
 TODOS = {
     'todo1': {'task': 'biuld an API'},
@@ -27,7 +34,7 @@ parser = reqparse.RequestParser()
 parser.add_argument('task')
 
 api.add_resource(TodoList, '/todos')
-
+api.add_resource(Todo, '/todos/<todo_id>')   
 if __name__ == '__main__':
     app.run(debug=True)
               
--- imants/restapid/testy.py
+++ imants/restapid/testy.py
@@ -2,7 +2,10 @@
 from requests import get, post
 
 ret = get ('http://localhost:5000/todos')
-print("GET{{}}: {}".format(ret.status_code, ret.text))
+print("GET({}): {}".format(ret.status_code, ret.json()))
 
 ret = post ('http://localhost:5000/todos', data={'task': 'cooltask'})
-print("POST{{}}: {}".format(ret.status_code, ret.text))
+print("POST({}): {}".format(ret.status_code, ret.json()))
+
+ret = get('http://localhost:5000/todos/todo1')
+print("GET({}): {}".format(ret.status_code, ret.json()))
