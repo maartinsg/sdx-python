a58f3ba83dd453575b604d6b60a422d70ed04cb2
jithu jaison <jison1998jai@gmail.com>
2020-01-29 11:14:18 +0200

    checkinstall

---
+++ JithuJiaison/restapid/checkinstall.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+from flask import Flask
+from flask_restful import Resource, Api
+app = Flask(__name__)
+api = Api(app)
+class HelloWorld(Resource):
+    def get(self):
+        return {'hello': 'AAAAA'}
+    
+api.add_resource(HelloWorld, '/')
+
+if __name__ == '__main__':
+        app.run(debug=True)
--- JithuJiaison/subprocess.py
+++ JithuJiaison/subprocess.py
@@ -1,6 +1,5 @@
 from subprocess import Popen, PIPE
-
-process = Popen(""join(ping 8.8.8.8 -c 2),
+process = Popen(""join (ping 8.8.8.8 -c 2),
                 shell=True, stdout=PIPE, stderr=PIPE)
 print(process.stpout.read())
 
