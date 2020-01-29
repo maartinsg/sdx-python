13d240e1cc2becdeceacbfc7b146ef04e2826838
Imants Purins <itsimants@gmail.com>
2020-01-29 12:37:37 +0200

    imants-feature-1

--- imants/restapid/testy.py
+++ imants/restapid/testy.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-from requests import get, put, post
+from requests import get, put, post, delete
 
 ret = put (
     'http://localhost:5000/todos/todo10',
@@ -15,4 +15,12 @@ print("POST({}): {}".format(ret.status_code, ret.json()))
 ret = get('http://localhost:5000/todos/todo1')
 print("GET({}): {}".format(ret.status_code, ret.json()))
 
+ret = delete('http://localhost:5000/todos/todo1')
+print("DELETE({}): {}".format(
+    ret.status_code,
+    ret.text))
 
+ret = get('http://localhost:5000/todos')
+print("GET({}): {}".format(
+    ret.status_code,
+    ret.json()))
