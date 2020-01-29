9f091ecc2f9e09bf97adcccc9038a09c565216d1
Anna D <dobritskaya39.ab@gmail.com>
2020-01-29 12:20:52 +0200

    the calling element from the collection added

--- Anna/restapid/rest.py
+++ Anna/restapid/rest.py
@@ -4,7 +4,7 @@ from flask import Flask
 from flask_restful import reqparse, abort, Api, Resource
 
 #todo list
-#shows a list of all todos and lets you POST to add new tasks
+#shows a list of all todos and lets you POST to add new tasks / collection 
 class TodoList(Resource):
     def get(self):
         print("debug: sending full list")
@@ -18,6 +18,12 @@ class TodoList(Resource):
         print("sebug: added task with id '{}'".format(todo_id))
         return TODOS[todo_id], 201
 
+# shows a single todo item and lets you delete a todo item /elements
+class Todo(Resource):
+    def get(self, todo_id):
+        #abort_if_todo_doesnt_exist(todo_id)
+        return TODOS[todo_id]
+
 TODOS = {
     'todo1': {'task': 'build an API'},
     'todo2': {'task': '?????'}
@@ -30,6 +36,7 @@ parser = reqparse.RequestParser()
 parser.add_argument('task')
 
 api.add_resource(TodoList, '/todos')
+api.add_resource(Todo, '/todos/<todo_id>')
 
 if __name__ == '__main__':  #if smb use console -> open app
     app.run(debug=True)
--- Anna/restapid/test.py
+++ Anna/restapid/test.py
@@ -1,7 +1,10 @@
 from requests import get, post
 
 ret = get('http://localhost:5000/todos')
-print("GET({}): {}".format(ret.status_code, ret.json()))
+print("GET({}): {}".format(ret.status_code, ret.text))
 
 ret = post('http://localhost:5000/todos', data={'task': 'cool task'})
 print("POST({}): {}".format(ret.status_code, ret.text))
+
+ret = get('http://localhost:5000/todos/todo1')
+print("GET({}): {}".format(ret.status_code, ret.json()))
