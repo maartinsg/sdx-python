a99d43f920ed9228d547e95d1ce26424c0ef78cb
vladislavs ignatenkovs <phontyz@gmail.com>
2020-01-29 12:40:41 +0200

    Added version 5 delete

--- vlad/restapid/rest.py
+++ vlad/restapid/rest.py
@@ -38,6 +38,12 @@ class Todo(Resource):
         #abort_if_todo_doesnt_exist(todo_id)
         return TODOS[todo_id]
 
+    def delete(self, todo_id):
+        print("debug: deleting task with id '{}'".format(todo_id))
+        #abort_if_todo_doesnt_exist(todo_id)
+        del TODOS[todo_id]
+        return '', 204
+
     def put(self, todo_id):
         args = parser.parse_args()
         task = {'task': args['task']}
--- vlad/restapid/rest_client.py
+++ vlad/restapid/rest_client.py
@@ -1,4 +1,4 @@
-from requests import get, post, put
+from requests import get, post, put, delete
 
 
 #ret = get('http://localhost:5000/todos')
@@ -7,10 +7,20 @@ from requests import get, post, put
 #ret = post('http://localhost:5000/todos', data={'task': 'u can do better!!!!@!@!'})
 #print("POST({}): {}".format(ret.status_code, ret.json()))
 
-ret = put(
-    'http://localhost:5000/todos/todo10',
-    data={'task': 'something cool'}
-    )
+#ret = put(
+#    'http://localhost:5000/todos/todo10',
+ #   data={'task': 'something cool'}
+#    )
+
+#ret = get('http://localhost:5000/todos')
+#print("GET({}): {}".format(ret.status_code, ret.text))
+
+ret = delete('http://localhost:5000/todos/todo2')
+print("DELETE({}): {}".format(
+    ret.status_code,
+    ret.text))
 
 ret = get('http://localhost:5000/todos')
-print("GET({}): {}".format(ret.status_code, ret.text))
+print("GET({}): {}".format(
+    ret.status_code,
+    ret.json()))
