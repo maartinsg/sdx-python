3309f051bcb5f60b5c45dbd78ce14e1161a96304
Ainars Palcevskis <ainars.palcevskis@gmail.com>
2020-01-29 12:41:59 +0200

    final vers

--- ainars/restapid/test.py
+++ ainars/restapid/test.py
@@ -1,5 +1,5 @@
 #!/user/bin/env python
-from requests import post, get, put
+from requests import post, get, put, delete
 
 ret = get('http://localhost:5000/todos')
 print("GET({}): {}".format(ret.status_code, ret.json()))
@@ -9,6 +9,12 @@ print("POST({}): {}".format(ret.status_code, ret.json()))
 
 ret = put('http://localhost:5000/todos/todo10', data={'task': 'something cool'})
 print("GET({}): {}".format(ret.status_code, ret.json()))
+
+ret = get('http://localhost:5000/todos')
+print("GET({}): {}".format(ret.status_code, ret.json()))
+
+ret = delete('http://localhost:5000/todos/todo10')
+print("DELETE({}): {}".format(ret.status_code,ret.text))
 
 ret = get('http://localhost:5000/todos')
 print("GET({}): {}".format(ret.status_code, ret.json()))
