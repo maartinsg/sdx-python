f0459d159689c60f96756e43cb5a9586b3e68841
Martins Galenieks tested <sourcetester@gmail.com>
2020-01-29 14:16:01 +0200

    v5?????? rest.py added coded text

--- maartinsg/restapid/rest.py
+++ maartinsg/restapid/rest.py
@@ -33,31 +33,44 @@ parser.add_argument('task')
 ## actually setting up the Api resource routing here
 #api.add_resource(TodoList, '/todos')
 
-#class TodoSimple(Resource):
-#    def get(self, todo_id):
-#        return {todo_id: todos[todo_id]}
-#
-#    def put(selfself, todo_id):
-#        todos[todo_id] = request.form['data']
-#        return {todo_id: todos[todo_id]}
+class TodoSimple(Resource):
+    def get(self, todo_id):
+        return {todo_id: todos[todo_id]}
+
+    def put(selfself, todo_id):
+        todos[todo_id] = request.form['data']
+        return {todo_id: todos[todo_id]}
 
 # return Dictionary part
-#api.add_resource(TodoList, '/todos')
+######api.add_resource(TodoSimple, '/todos')
 
 class Todo(Resource):
     def get(self, todo_id):
-        abort_if_todo_doesnt_exist(todo_id)
+        print("debug: getting task with the id '{}'".format(todo_id))
+        #abort_if_todo_doesnt_exist(todo_id)
         return TODOS[todo_id]
 
     def put(self, todo_id):
+        print("debug: deleting the task with the id '{}'".format(todo_id))
         args = parser.parse_args()
         task = {'task': args['task']}
         TODOS[todo_id] = task
+        return task, 201
+
+    def delete(self, todo_id):
+        print("debug: deleting task with the id '{}'".format(todo_id))
+        #abort_if_todo_doesnt_exist(todo_id)
+        del TODOS[todo_id]
+        return '', 204
+
+
+
+
+
+## actually setting up the Api resource routing here
+api.add_resource(TodoList, '/todos')
 
 api.add_resource(Todo, '/todos/<todo_id>')
 
-## actually setting up the Api resource routing here
-######api.add_resource(TodoList, '/todos/<todo_id>')
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=12345)
--- maartinsg/restapid/test.py
+++ maartinsg/restapid/test.py
@@ -1,15 +1,28 @@
 #!/usr/bin/env python
 
-from requests import put, get, post
+from requests import put, get, post, delete
 
 ret = put(
-    'http://localhost:5000/todos/todo10',
+    'http://localhost:12345/todos/todo10',
     data={'task': 'maybe to create a datafile please'}
     )
 print("PUT({}): {}".format(ret.status_code, ret.text))
 
-ret = get('http://localhost:5000/todos/todos')
-print("GET({}): {}".format(ret.status_code, ret.text))
+ret = delete('http://127.0.0.1:12345/todos/todo10')
+print("DELETE({}): {}".format(
+    ret.status_code,
+    ret.text))
+
+ret = get('http://127.0.0.1:12345/todos/todo10')
+print("GET({}): {}".format(
+    ret.status_code,
+    ret.text))
+
+
+
+
+
+
 
 #ret = put(
 #    'http://localhost:5000/todo2',
