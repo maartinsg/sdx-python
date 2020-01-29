38dfd7b7c5d86310ce707d89df38b9269cfd8a94
Ainars Palcevskis <ainars.palcevskis@gmail.com>
2020-01-29 13:02:26 +0200

    checked connection with friends

--- ainars/restapid/rest.py
+++ ainars/restapid/rest.py
@@ -57,4 +57,4 @@ api.add_resource(Todo, '/todos/<todo_id>')
 api.add_resource(TodoList, '/todos')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
--- ainars/restapid/test.py
+++ ainars/restapid/test.py
@@ -1,7 +1,7 @@
 #!/user/bin/env python
 from requests import post, get, put, delete
 
-ret = get('http://localhost:5000/todos')
+ret = get('http://192.168.8.107:5000/todos')
 print("GET({}): {}".format(ret.status_code, ret.json()))
 
 ret = post('http://localhost:5000/todos', data={'task': 'cool task'})
