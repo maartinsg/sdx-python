ad43cea2f3480d235fd49e6ab91ad7ab4d697a7f
vladislavs ignatenkovs <phontyz@gmail.com>
2020-01-29 12:55:46 +0200

    FINAL

--- vlad/restapid/rest.py
+++ vlad/restapid/rest.py
@@ -58,4 +58,5 @@ api.add_resource(TodoSimple, '/todos')
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port='5000')
+#192.168.8.107
--- vlad/restapid/rest_client.py
+++ vlad/restapid/rest_client.py
@@ -7,20 +7,22 @@ from requests import get, post, put, delete
 #ret = post('http://localhost:5000/todos', data={'task': 'u can do better!!!!@!@!'})
 #print("POST({}): {}".format(ret.status_code, ret.json()))
 
-#ret = put(
-#    'http://localhost:5000/todos/todo10',
- #   data={'task': 'something cool'}
-#    )
+ret = put(
+    'http://localhost:5000/todos/todo10',
+    data={'task': 'something cool'}
+    )
 
 #ret = get('http://localhost:5000/todos')
 #print("GET({}): {}".format(ret.status_code, ret.text))
 
-ret = delete('http://localhost:5000/todos/todo2')
-print("DELETE({}): {}".format(
-    ret.status_code,
-    ret.text))
+#ret = delete('http://192.168.8.108:5000/todos/todo2')
+#print("DELETE({}): {}".format(
+  #  ret.status_code,
+  #  ret.text))
 
-ret = get('http://localhost:5000/todos')
+ret = get('http://192.168.8.108:5000/todos')
 print("GET({}): {}".format(
     ret.status_code,
     ret.json()))
+
+# 192.168.8.108
