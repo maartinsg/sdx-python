8f27b14bb16a8f2870772ca88922771a5b199af0
Martins Galenieks tested <sourcetester@gmail.com>
2020-01-29 11:59:33 +0200

    changed letters in the maartinsg/restapid/test.py and the rest.py files added for testing only again

--- maartinsg/restapid/test.py
+++ maartinsg/restapid/test.py
@@ -1,21 +1,21 @@
 #!/usr/bin/env python
 
-from requests import put, get
+from requests import put, get, post
 
-ret = put(
-    'http://localhost:5000/todo1',
-    data={'data': 'Remember the milk please'}
-    )
-print("PUT(todo1): {}".format(ret.test()))
+#ret = put(
+#    'http://localhost:5000/todo1',
+#    data={'data': 'Remember the milk please'}
+#    )
+#print("PUT(todo1): {}".format(ret.json()))
 
-ret = get('http://localhost:5000/todo1')
-print("GET(todo1): {}".format(ret.test()))
+ret = get('http://localhost:5000/todos')
+print("GET(todo1): {}".format(ret.json()))
 
-ret = put(
-    'http://localhost:5000/todo2',
-    data={'data': 'Change my brakepads please'}
-    )
-print("PUT(todo2): {}".format(ret.test()))
+#ret = put(
+#    'http://localhost:5000/todo2',
+#    data={'data': 'Change my brakepads please'}
+#    )
+#print("PUT(todo2): {}".format(ret.json()))
 
-ret = get('http://localhost:5000/todo2')
-print("GET(todo2): {}".format(ret.test()))
+ret = post('http://localhost:5000/todos')
+print("POST(todos): {}".format(ret.json()))
