5abf68c9f029dbc23683f782aea3558577daf868
jithu jaison <jison1998jai@gmail.com>
2020-01-29 13:59:39 +0200

    p1

--- JithuJiaison/restapid/p1.py
+++ JithuJiaison/restapid/p1.py
@@ -1,4 +1,4 @@
-#!/user/bin/env python
+
 from flask import Flask, request
 from flask_restful import reqparse, abort, Resource, Api
 app = Flask(__name__)
@@ -6,8 +6,9 @@ api = Api(app)
 
     
 TODOS = {
-    'todo1': {'task': 'build an API'),
-    'todo2': {'task': '?????'),
+    'todo1': {'task': 'build an API'},
+    'todo2': {'task': '?????'},
+    'todo3': {'task': 'profit!'},
     }
 parser = reqparse.RequestPaeser()
 parser.add_argument('task')
@@ -19,10 +20,11 @@ class Todo(Resource):
         return TODOS[todo_id]
     
      def delete(self, todo_id):
-        print("debug: deleting task with id '[]'".format(todo_id))
+        print("debug: deleting task with id '{}'".format(todo_id))
         abort_if_todo_doesnt_exist(todo_id)
         del TODOS[todo_id]
         return '', 204
+    
     def put(self, todo_id):
         args = parser.parse_args()
         task = {'task': args['task']}
