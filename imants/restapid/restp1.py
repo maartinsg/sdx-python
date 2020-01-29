08cded1ecd7861dc152ee51ec8114fb74a923b2d
Imants Purins <itsimants@gmail.com>
2020-01-29 12:40:59 +0200

    imants-feature-1

--- imants/restapid/restp1.py
+++ imants/restapid/restp1.py
@@ -19,7 +19,16 @@ class Todo(Resource):
     def get(self, todo_id):
         # abort_if_todo_doesnt_exist(todo_id)
         return TODOS[todo_id]
-    
+    def delete(self, todo_id):
+        print("debug: deleting task with id'{}'".format(todo_id))
+        #abort_if_todo_doesnt_exit(todo_id)
+        del TODOS[todo_id]
+        return '', 204
+    def put(self, todo_id):
+        args = parser.parse_args()
+        task = {'task': args['task']}
+        TODOS[todo_id] = task
+        return task, 201
  
     
 TODOS = {
