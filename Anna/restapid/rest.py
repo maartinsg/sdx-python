632d0c0637cbafef314fa2e7d6b287258add3a2d
Anna D <dobritskaya39.ab@gmail.com>
2020-01-29 11:40:12 +0200

    client - test.py, server - rest.py

--- Anna/restapid/rest.py
+++ Anna/restapid/rest.py
@@ -1,16 +1,22 @@
 #!/usr/bin/env python
 
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
 
-app = Flask(__name__) # __name__ - name of file, reserved one
-api = Api(app)
+app = Flask(__name__) # __name__ - name of file, reserved one/ initialize flask(server)
+api = Api(app) #initialize api(handler for the requests)
 
-class HelloWorld(Resource):
-    def get(self):
-        return {'hello':'world'}
+todos = {}
 
-api.add_resource(HelloWorld, '/')
+class TodoSimple(Resource):
+    def get(self, todo_id):
+        return {todo_id: todos[todo_id]}
+
+    def put(self, todo_id):
+        todos[todo_id] = request.form['data']
+        return {todo_id: todos[todo_id]}
+
+api.add_resource(TodoSimple, '/<string:todo_id>')
 
 if __name__ == '__main__':  #if smb use console -> open app
     app.run(debug=True)
