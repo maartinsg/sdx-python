77b4597841a2e9e7b7e93be6b4ff189dcc9ab670
vladislavs ignatenkovs <phontyz@gmail.com>
2020-01-29 11:40:06 +0200

    Added server and client scripts and fixed few typos

---
+++ vlad/restapid/rest_client.py
@@ -0,0 +1,19 @@
+from requests import put, get
+
+ret = put(
+    'http://localhost:5000/todo1',
+    data={'data': 'Remember the milk'}
+    )
+print("PUT(todo1): {}".format(ret.json()))
+
+ret = get('http://localhost:5000/todo1')
+print("GET(todo1): {}".format(ret.json()))
+
+ret = put(
+    'http://localhost:5000/todo2',
+    data={'data': 'change my breakpads'}
+    )
+print("PUT(todo2): {}".format(ret.json()))
+
+ret = get('http://localhost:5000/todo2')
+print("GET(todo2): {}".format(ret.json()))
