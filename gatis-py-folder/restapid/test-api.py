72c700d6a1b8f39778cc0bf8d534dafb43bdc662
Gatis <student@ubuntu.com>
2020-01-30 12:08:26 +0200

    adding Dockerfile

---
+++ gatis-py-folder/restapid/test-api.py
@@ -0,0 +1,17 @@
+from requests import post, get, put, delete
+
+##ret = put(
+##    'http://localhost:5000/todos/todo15',
+##    data={'task': 'somthing coool 15'}
+##    )
+##print("PUT({}): {}".format(ret.status_code, ret.json()))
+
+##ret = delete('http://localhost:5000/todos/todo15')
+##print("DELETE({}): {}".format(ret.status_code, ret.text))
+
+##ret = get('http://localhost:5000/todos')
+##print("GET({}): {}".format(ret.status_code, ret.json()))
+
+ret = get('http://localhost:5000/ping/8.8.8.8')
+print("GET({}): {}".format(ret.status_code, ret.json()))
+
