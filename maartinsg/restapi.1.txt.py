38a1ec0f0f1fb70c85100c7f824be20d42df45ea
maartinsg <60295033+maartinsg@users.noreply.github.com>
2020-01-30 18:33:15 +0200

    Create restapi.1.txt.py
    restapi as a code maybe.

---
+++ maartinsg/restapi.1.txt.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python
+
+####################
+# RestAPI
+####################
+from subprocess import Popen, PIPE
+from flask import Flask, request
+from flask_restful import reqparse, abort, Resource, Api
+
+class TodoList(Resource):
+    def get(self):
+        print("debug: sending full list")
+        return TODOS
+
+    def post(self):
+        args = parser.parse_args()
+        todo_id = int(max(TODOS.keys()).lstrip('todo')) + 1
+        todo_id = 'todo%i' % todo_id
+        TODOS[todo_id] = {'task': args['task']}
+        print("debug: added task with id '{}'".format(todo_id))
+        return TODOS[todo_id], 201
+
+TODOS = {
+    'todo1': {'task': 'build an API'},
+    'todo2': {'task': '?????'}
+}
+
+app = Flask(__name__)
+api = Api(app)
+
+parser = reqparse.RequestParser()
+parser.add_argument('task')
+
+## actually setting up the Api resource routing here
+#api.add_resource(TodoList, '/todos')
+
+class TodoSimple(Resource):
+    def get(self, todo_id):
+        return {todo_id: todos[todo_id]}
+
+    def put(self, todo_id):
+        todos[todo_id] = request.form['data']
+        return {todo_id: todos[todo_id]}
+
+# return Dictionary part
+######api.add_resource(TodoSimple, '/todos')
+
+class Ping(Resource):
+    def get(self, ip):
+        print("debug: ping IP '{}'".format(ip))
+        process = Popen(" ".join(['ping', '-c', '2', ip]), shell=True, stdout=PIPE, stderr=PIPE)
+        #print(processing)
+        #read STDOUT
+        out = process.stdout.read()
+        #wait for the subprocess to finish?
+        rc = process.wait()
+        print("exit code: {}".format(rc))
+        # print("error: {}".format(process.stderr.read()))
+        return out
+
+#read STDERR
+
+
+class Todo(Resource):
+    def get(self, todo_id):
+        print("debug: getting task with the id '{}'".format(todo_id))
+        #abort_if_todo_doesnt_exist(todo_id)
+        return TODOS[todo_id]
+
+    def put(self, todo_id):
+        print("debug: put as a post? '{}'".format(todo_id))
+        args = parser.parse_args()
+        task = {'task': args['task']}
+        TODOS[todo_id] = task
+        return task, 201
+
+    def delete(self, todo_id):
+        print("debug: deleting task with the id '{}'".format(todo_id))
+        #abort_if_todo_doesnt_exist(todo_id)
+        del TODOS[todo_id]
+        return '', 204
+
+
+
+
+class ObtainStudentData(Resource):
+    def post(self, student_id):
+        data = request.form.to_dict()
+        print("Data ({})".format(data))
+        return data, 201
+
+    def get(self, student_id):
+        print("debug: getting task with the id '{}'".format(student_id))
+        #abort_if_todo_doesnt_exist(todo_id)
+        return TODOS[student_id], 200
+
+## actually setting up the Api resource routing here
+api.add_resource(TodoList, '/todos')
+
+api.add_resource(Todo, '/todos/<todo_id>')
+
+api.add_resource(Ping, '/ping/<ip>')
+
+api.add_resource(ObtainStudentData, '/studentid/<student_id>')
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=12345, threaded=True)
