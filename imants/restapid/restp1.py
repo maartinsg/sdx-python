19cfb76f7bc96d26d8992bc45fd1b1de0bf0c900
Imants Purins <itsimants@gmail.com>
2020-01-30 09:45:46 +0200

    imants-feature-1

--- imants/restapid/restp1.py
+++ imants/restapid/restp1.py
@@ -45,5 +45,5 @@ parser.add_argument('task')
 api.add_resource(TodoList, '/todos')
 api.add_resource(Todo, '/todos/<todo_id>')   
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
               
--- imants/restapid/testy.py
+++ imants/restapid/testy.py
@@ -1,26 +1,28 @@
 #!/usr/bin/env python
 from requests import get, put, post, delete
 
-ret = put (
-    'http://localhost:5000/todos/todo10',
-    data={'task': 'something cool'}
-    )
-print("PUT({}): {}".format(ret.status_code, ret.json()))
-ret = get ('http://localhost:5000/todos')
+#ret = post ('http://localhost:5000/todos', data={'task': 'cooltask'})
+#print("POST({}): {}".format(ret.status_code, ret.json()))
+
+#ret = put (
+    #'http://localhost:5000/todos/todo10',
+    #data={'task': 'something cool'}
+   # )
+#print("PUT({}): {}".format(ret.status_code, ret.json()))
+
+ret = get ('http://192.168.8.105:5000/TODOS')
 print("GET({}): {}".format(ret.status_code, ret.json()))
 
-ret = post ('http://localhost:5000/todos', data={'task': 'cooltask'})
-print("POST({}): {}".format(ret.status_code, ret.json()))
+#ret = delete('http://localhost:5000/todos/todo10')
+#print("DELETE({}): {}".format(
+    #ret.status_code,
+    #ret.text))
 
-ret = get('http://localhost:5000/todos/todo1')
-print("GET({}): {}".format(ret.status_code, ret.json()))
+#ret = get ('http://localhost:5000/TODOS')
+#print("GET({}): {}".format(ret.status_code, ret.json()))
 
-ret = delete('http://localhost:5000/todos/todo1')
-print("DELETE({}): {}".format(
-    ret.status_code,
-    ret.text))
-
-ret = get('http://localhost:5000/todos')
-print("GET({}): {}".format(
-    ret.status_code,
-    ret.json()))
+#ret = put (
+   # 'http://localhost:5000/todos/todo10',
+   # data={'task': 'something cool'}
+    #)
+#print("PUT({}): {}".format(ret.status_code, ret.json()))
