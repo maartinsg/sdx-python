2b15c7ec9b6f971c7e36cab25b10bf6ee4976213
Karlis Valainis <val.karlis@gmail.com>
2020-01-30 14:02:20 +0200

    fixed code

--- karlis-CABC/jev-sdx-restapi/app/main.py
+++ karlis-CABC/jev-sdx-restapi/app/main.py
@@ -13,6 +13,7 @@ TODOS = {
     'todo1': {'task': 'build an API'},
     'todo2': {'task': 'do something good ;-)'},
     'todo3': {'task': 'profit!'},
+    'todo4': {'task': '???'},
 }
 
 
@@ -20,12 +21,13 @@ def abort_if_todo_doesnt_exist(todo_id):
     if todo_id not in TODOS:
         abort(404, message="Todo {} doesn't exist".format(todo_id))
 
+
 parser = reqparse.RequestParser()
 parser.add_argument('task')
-
-
 # Todo
 # shows a single todo item and lets you delete a todo item
+
+
 class Todo(Resource):
     def get(self, todo_id):
         abort_if_todo_doesnt_exist(todo_id)
@@ -56,13 +58,14 @@ class TodoList(Resource):
         TODOS[todo_id] = {'task': args['task']}
         return TODOS[todo_id], 201
 
-##
-## Actually setup the Api resource routing here
-##
+#
+# Actually setup the Api resource routing here
+#
+
+
 api.add_resource(TodoList, '/todos')
 api.add_resource(Todo, '/todos/<todo_id>')
 
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=12345)
-
